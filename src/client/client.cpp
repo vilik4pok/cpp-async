a5629659e1e58e542fbdfb3b797228f16d2bd721
Альков Валентин Сергеевич <valkov@nic.etu>
2023-06-13 21:44:21 +0300

    update client.cpp

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,26 +1,47 @@
-//
-// blocking_tcp_echo_client.cpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2022 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/program_options.hpp>
 #include <thread>
-//#include <memory>
-//#include <string.h>
-using boost::asio::ip::tcp;
 
 enum { max_length = 1024 };
 
+bool process_command_line(int argc, char** argv, std::string& ip, size_t& port, size_t& threads, size_t& write_read) {
+    try
+    {
+        boost::program_options::options_description desc("Usage:\n  parallel_executor (-h | --help)\n  parallel_executor <ip> <port> <threads> <write_read>\nOptions:");
+        desc.add_options()
+            ("help,h", "Help screen")
+            ("ip,i", boost::program_options::value<std::string>(&ip)->required(), "Server ip")
+            ("port,p", boost::program_options::value <size_t>(&port)->required(), "Server port")
+            ("threads,t", boost::program_options::value<size_t>(&threads)->required(), "Thread count = client count")
+            ("write_read,w", boost::program_options::value<size_t>(&write_read)->required(), "Write-read count");
+        boost::program_options::positional_options_description pos_desc;
+        pos_desc.add("ip", 1);
+        pos_desc.add("port", 1);
+        pos_desc.add("threads", 1);
+        pos_desc.add("write_read", 1);
+        boost::program_options::command_line_parser parser(argc, argv);
+        parser.options(desc).positional(pos_desc);
+        boost::program_options::parsed_options parsed_options = parser.run();
+        boost::program_options::variables_map vm;
+        store(parsed_options, vm);
+        if (vm.count("help") || argc == 1) {
+            std::cout << desc << '\n';
+            return false;
+        }
+        notify(vm);
+    }
+    catch (const boost::program_options::error& ex)
+    {
+        std::cerr << ex.what() << '\n';
+        return false;
+    }
+    return true;
+}
 
-void send_requests(tcp::resolver::results_type ip, size_t count, size_t id) {
+void send_requests(boost::asio::ip::tcp::resolver::results_type ip, size_t count, size_t id) {
     {
         std::ostringstream ss;
         ss << "thread " << id << ": started\n";
@@ -28,7 +49,7 @@ void send_requests(tcp::resolver::results_type ip, size_t count, size_t id) {
     }
     try {
         boost::asio::io_context io_context;
-        tcp::socket s(io_context);
+        boost::asio::ip::tcp::socket s(io_context);
         boost::asio::connect(s, ip);
         char request[max_length] = "12345";
         char response[max_length];
@@ -55,20 +76,19 @@ void send_requests(tcp::resolver::results_type ip, size_t count, size_t id) {
 
 int main(int argc, char* argv[])
 {
-    if (argc < 3)
-        return 0;
-    size_t thread_count = 5;
-    size_t read_write_count = 10;
-    boost::asio::io_context io_context;
-    tcp::resolver resolver(io_context);
-    std::list<std::thread> threads(thread_count);
-    auto ip = resolver.resolve(argv[1], argv[2]);
-    for (size_t i = 1; i < thread_count + 1; i++) {
-        threads.push_back(std::thread(send_requests, ip, read_write_count, i));
-    }
-    for (auto& t : threads) {
-        t.join();
-    }
-
+    std::string ip;
+    size_t port, thread_count, write_read;
+    if (process_command_line(argc, argv, ip, port, thread_count, write_read)) {
+        boost::asio::io_context io_context;
+        boost::asio::ip::tcp::resolver resolver(io_context);
+        std::list<std::thread> threads(thread_count);
+        auto address = resolver.resolve(ip, std::to_string(port));
+        for (size_t i = 1; i < thread_count + 1; i++) {
+            threads.push_back(std::thread(send_requests, ip, write_read, i));
+        }
+        for (auto& t : threads) {
+            t.join();
+        }
+    };
     return 0;
 }
