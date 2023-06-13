c841d0cbe66aa492fb419633fa3fd538abf0740b
Альков Валентин Сергеевич <valkov@nic.etu>
2023-06-13 21:43:59 +0300

    update async_server.cpp

--- src/async_server/async_server.cpp
+++ src/async_server/async_server.cpp
@@ -1,19 +1,8 @@
-//
-// async_tcp_echo_server.cpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2022 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
 #include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <utility>
 #include <boost/asio.hpp>
-//#include <google/protobuf/descriptor.h>
 using boost::asio::ip::tcp;
 
 class session
@@ -106,17 +95,13 @@ int main(int argc, char* argv[])
             std::cerr << "Usage: async_tcp_echo_server <port>\n";
             return 1;
         }
-
         boost::asio::io_context io_context;
-
         server s(io_context, std::atoi(argv[1]));
-
         io_context.run();
     }
     catch (std::exception& e)
     {
         std::cerr << "Exception: " << e.what() << "\n";
     }
-
     return 0;
 }
